2126bfee022b3e44cc0a7555a9d4471345b74ec7
confett0 <118047961+confett0@users.noreply.github.com>
2024-10-17 20:50:50 +0200

    Style order confirmation modal

--- src/components/OrderConfirmation.jsx
+++ src/components/OrderConfirmation.jsx
@@ -1,30 +1,38 @@
 import { Link } from "react-router-dom";
 
 export default function OrderConfirmation({ cart, closeModal }) {
-    const firstCartItem = cart[0];
-    const shipping = 50;
-    const grandTotal = cart.reduce((a, b) => a + b.price * b.quantity, 0) + shipping;
+  const firstCartItem = cart[0];
+  const shipping = 50;
+  const grandTotal =
+    cart.reduce((a, b) => a + b.price * b.quantity, 0) + shipping;
 
   return (
     <div className="order-confirmation">
       <img src="src/assets/checkout/icon-order-confirmation.svg" />
-      <h3>Thank you <br />for your order</h3>
+      <h3>
+        Thank you <br />
+        for your order
+      </h3>
       <p>You will receive an email confirmation shortly.</p>
       <div className="order-summary">
         <div className="order-summary-items">
-            <div className="ordered-items-wrap">
-                <img src={firstCartItem.image.mobile} />
-                <div>
-                  <p>{firstCartItem.shortName}</p>
-                  <p>${firstCartItem.price}</p>
-                </div>
-                <p>x{firstCartItem.quantity}</p>
+          <div className="ordered-items-wrap">
+            <img src={firstCartItem.image.mobile} />
+            <div>
+              <p className="ordered-items-name">{firstCartItem.shortName}</p>
+              <p className="ordered-items-price">${firstCartItem.price}</p>
             </div>
-            {cart.length > 1 && <p>and {cart.length - 1} other item(s)</p>}
+            <p className="ordered-items-quantity">x{firstCartItem.quantity}</p>
+          </div>
+          {cart.length > 1 && (
+            <div className="order-summary-other-items">
+              <p>and {cart.length - 1} other item(s)</p>
+            </div>
+          )}
         </div>
         <div className="grand-total">
           <p>Grand Total</p>
-          <p>{grandTotal}</p>
+          <p className="order-confirmation-grandtotal">${grandTotal}</p>
         </div>
       </div>
       <Link to="/">
