0c0d3f2cec821e724c92579d5dab039eb529c2c8
confett0 <118047961+confett0@users.noreply.github.com>
2024-12-10 18:08:58 +0100

    Wrap navlinks in <li> elements

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,46 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Nav() {
+  const [showNav, setShowNav] = useState(false);
+  const toggleNav = () => setShowNav((prev) => !prev);
+
   return (
     <nav>
-      <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
-        Home
-      </NavLink>
-      <NavLink
-        to="shop/headphones"
-        className={({ isActive }) => (isActive ? "active" : "")}
-      >
-        Headphones
-      </NavLink>
-      <NavLink
-        to="shop/speakers"
-        className={({ isActive }) => (isActive ? "active" : "")}
-      >
-        Speakers
-      </NavLink>
-      <NavLink
-        to="shop/earphones"
-        className={({ isActive }) => (isActive ? "active" : "")}
-      >
-        Earphones
-      </NavLink>
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="shop/headphones"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Headphones
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="shop/speakers"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Speakers
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="shop/earphones"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Earphones
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
