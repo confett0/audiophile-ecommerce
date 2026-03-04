60bedf57530933c0a58e4d87924df9b5bdcfe751
confett0 <118047961+confett0@users.noreply.github.com>
2026-03-04 19:11:08 +0100

    Add more action types to reducer

--- src/cartReducer.ts
+++ src/cartReducer.ts
@@ -1,23 +1,57 @@
 import type { CartItem } from "./types/cart.js";
 
-const cartReducer = (state: CartItem[], action: (a: CartItem) => void) => {
+const cartReducer = (state: CartItem[], action: CartAction) => {
   switch (action.type) {
-    case "add item":
-      return [
-        ...state,
-        {
-          id: action.payload.id,
-          name: action.payload.name,
-          shortName: action.payload.shortName,
-          image: action.payload.image,
-          price: action.payload.price,
-        },
-      ];
-    case "remove item":
-      return state.filter((item) => item.id !== action.payload);
+    case "ADDED_ITEM":
+      const existing = state.find((item) => item.id === action.payload.id);
+      if (existing)
+        return state.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, quantity: item.quantity + action.quantity }
+            : item,
+        );
+      else
+        return [
+          ...state,
+          {
+            id: action.payload.id,
+            name: action.payload.name,
+            shortName: action.payload.shortName,
+            image: action.payload.image,
+            price: action.payload.price,
+            quantity: action.quantity,
+          },
+        ];
+    case "REMOVED_ITEM":
+      return state.filter((item) => item.id !== action.payload.id);
+    case "INCREMENTED_QUANTITY":
+      return state.map((item) =>
+        item.id === action.payload.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item,
+      );
+    case "DECREMENTED_QUANTITY":
+      return state.map((item) =>
+        item.id === action.payload.id
+          ? { ...item, quantity: item.quantity - 1 }
+          : item,
+      );
+    case "EMPTIED_CART":
+      return [];
     default:
       return state;
   }
+};
+
+type CartAction = {
+  type:
+    | "ADDED_ITEM"
+    | "REMOVED_ITEM"
+    | "INCREMENTED_QUANTITY"
+    | "DECREMENTED_QUANTITY"
+    | "EMPTIED_CART";
+  payload: CartItem;
+  quantity: number;
 };
 
 export default cartReducer;
