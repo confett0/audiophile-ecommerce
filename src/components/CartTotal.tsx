8f9e50950a601a35e8b202847a38999459e91ab0
confett0 <118047961+confett0@users.noreply.github.com>
2026-03-11 17:05:51 +0100

    Extract CartTotal component

---
+++ src/components/CartTotal.tsx
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import CartContext from "../CartContext.js";
+import type { CartItem } from "../types/cart.js";
+
+export default function CartTotal({
+  showDetails = false,
+}: {
+  showDetails: boolean;
+}) {
+  const { cart } = useContext(CartContext);
+
+  const orderTotal = cart.reduce(
+    (a: number, b: CartItem) => a + b.price * b.quantity,
+    0,
+  );
+  const shipping = 50;
+  const vatRate = 0.2; // 20% VAT
+  const vat = (orderTotal * vatRate).toFixed(0);
+  const grandTotal = orderTotal + shipping;
+  return (
+    <div className="cart-total-wrap">
+      <p>TOTAL</p>
+      <p className="cart-total">${orderTotal}</p>
+      {showDetails && (
+        <>
+          <p>SHIPPING</p>
+          <p className="cart-total">${shipping}</p>
+          <p>VAT (INCLUDED)</p>
+          <p className="cart-total">${vat}</p>
+          <p className="grandtotal">GRANDTOTAL</p>
+          <p className="cart-total grandtotal">${grandTotal}</p>
+        </>
+      )}
+    </div>
+  );
+}
