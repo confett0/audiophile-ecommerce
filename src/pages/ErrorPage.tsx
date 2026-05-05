0a968d153e9dd035e17e3899ea3bc5ed9bef86ae
confett0 <118047961+confett0@users.noreply.github.com>
2026-05-05 16:54:28 +0200

    Add error message to error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,10 +1,28 @@
 import Layout from "../components/Layout";
 import CategoryLinks from "../components/CategoryLinks";
+import LinkButton from "../components/LinkButton";
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 
 export default function ErrorPage() {
+  const error = useRouteError();
+  let title = "Something went wrong";
+  let message = "An unexpected error occurred";
+
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 404) {
+      title = "Error 404";
+      message = "Page not found";
+    } else {
+      title = `Error ${error.status}`;
+      message = error.statusText;
+    }
+  }
+
   return (
     <Layout>
-      <h1>Errore</h1>
+      <h1>{title}</h1>
+      <p>{message}</p>
+      <LinkButton to="/">Go Home</LinkButton>
       <CategoryLinks />
     </Layout>
   );
